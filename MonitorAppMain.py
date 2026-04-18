ffcf7a5298c5b61df8bbf1cfe8b574bb9e201c25
Robusr <Robusr@outlook.com>
2026-04-19 05:38:55 +0800

    coding

---
+++ MonitorAppMain.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from MonitorMainWindow import MonitorMainWindow
+
+app = QApplication(sys.argv)
+icon = QIcon("MonitorQtApp/images/hammer.ico")
+app.setWindowIcon(icon)
+mainform = MonitorMainWindow()
+
+mainform.show()
+sys.exit(app.exec_())
+
+
